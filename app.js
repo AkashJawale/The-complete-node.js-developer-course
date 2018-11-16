8e5a4436d0a9ebd5d42988513882720b481ac13c
AkashJawale <akashjawale131@gmail.com>
2018-11-16 17:58:46 +0530

    remove all duplicates from array

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ console.log('Starting app.js');
 
 const fs = require('fs');
 const os = require('os');
+const _ = require('lodash');
 const notes = require('./notes.js');
 
-console.log('Result:', notes.add(9, -2));
+// console.log(_.isString(true));
+// console.log(_.isString('Akash'));
+
+var filteredArray =_.uniq(['Akash', 1, 'Akash', 1, 2, 3, 4]);
+console.log(filteredArray);
+//console.log('Result:', notes.add(9, -2));
 
 // var user = os.userInfo();
 
